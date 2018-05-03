0ece3c846484be7057b9b46e24ca0659ac0bffbc
mckeont <mckeont@upenn.edu>
2018-05-03 17:03:14 -0400

    Zoomie again?
    10 to 8

--- js/setup.js
+++ js/setup.js
@@ -1,6 +1,6 @@
 var map = L.map('map', {
   center: [39.95399727797304, -75.20959138870239],
-  zoom: 10,
+  zoom: 8,
 });
 
 var Stamen_TonerLite = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
